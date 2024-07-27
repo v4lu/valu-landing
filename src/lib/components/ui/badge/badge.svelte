8045ce28c4fa3701cb8e6be60e2002fbb05d5087
Luka <lukabrkovic@proton.me>
2024-07-27 20:23:29 +0200

    feat: temp landing

---
+++ src/lib/components/ui/badge/badge.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	let { children }: HTMLAttributes<HTMLDivElement> = $props();
+</script>
+
+<div
+	class="after:bg-hero-badge-gradient after:mask-hero-badge relative isolate mx-auto mb-4 mt-0 flex w-max items-center overflow-hidden rounded-[32px] py-1 pl-2 pr-[13px] shadow-badge-shadow backdrop-blur-[6px] transition-all ease-cubic after:pointer-events-none after:absolute after:inset-0 after:rounded-[inherit] after:p-px after:content-['']"
+>
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
---
+++ src/lib/components/ui/button/button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { type VariantProps } from 'class-variance-authority';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+	import { buttonVariants } from '.';
+	import { cn } from '$lib/cn';
+	type Variant = VariantProps<typeof buttonVariants>['variant'];
+	type Size = VariantProps<typeof buttonVariants>['size'];
+
+	type ButtonPropsType = HTMLButtonAttributes & {
+		class?: string;
+		variant?: Variant;
+		size?: Size;
+	};
+
+	let { class: className, variant, size, children, ...rest }: ButtonPropsType = $props();
+</script>
+
+<button {...rest} class={cn(buttonVariants({ variant, size, className }))}>
+	{#if children}
+		{@render children()}
+	{/if}
+</button>
