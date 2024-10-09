835bd567124bfd5303d4068eb3aad851c2735b1e
Luka <lukabrkovic@proton.me>
2024-10-09 18:44:06 +0200

    feat: docker

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,21 @@
 # Stage 1: Build stage
 FROM node:20-alpine AS builder
-
 RUN npm install -g pnpm
 WORKDIR /app
 COPY package.json pnpm-lock.yaml* ./
 RUN pnpm install --no-frozen-lockfile
-RUN rm -rf node_modules
 COPY . .
-RUN pnpm install --no-frozen-lockfile
 RUN pnpm run build
 RUN ls -la /app
 
 # Stage 2: Runtime stage
 FROM node:20-alpine
-
 RUN npm install -g pnpm
 WORKDIR /app
+COPY --from=builder /app/package.json /app/pnpm-lock.yaml* ./
 COPY --from=builder /app/build ./build
-COPY package.json pnpm-lock.yaml* ./
+COPY --from=builder /app/src ./src
 RUN pnpm install --prod --no-frozen-lockfile
-
 EXPOSE 3000
+ENV NODE_ENV=production
 CMD ["node", "build"]
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,18 @@
-import { join } from 'node:path';
 import { readFile, readdir } from 'node:fs/promises';
+import { join } from 'node:path';
 import type { PageServerLoad } from './$types';
 import type { Metadata, Post } from '$lib/types';
 
 export const load: PageServerLoad = async () => {
-	const postsDirectory = join('/app', 'src', 'blogs');
+	// eslint-disable-next-line node/prefer-global/process
+	const postsDirectory = join(process.cwd(), 'src/blogs');
 	const files = await readdir(postsDirectory);
+
 	const posts: Post[] = await Promise.all(
 		files.map(async (file) => {
 			const filePath = join(postsDirectory, file);
 			await readFile(filePath, 'utf-8');
-			const { metadata } = (await import(`../../blogs/${file}`)) as { metadata: Metadata };
+			const { metadata } = (await import(`../blogs/${file}`)) as { metadata: Metadata };
 			return {
 				slug: file.replace('.md', ''),
 				title: metadata.title,
@@ -21,13 +23,17 @@ export const load: PageServerLoad = async () => {
 			};
 		})
 	);
-	const sortedPosts = posts.sort((a, b) => {
-		const parseDate = (dateString: string) => {
-			const [day, month, year] = dateString.split('.').map(Number);
-			return new Date(year, month - 1, day).getTime();
-		};
-		return parseDate(b.date) - parseDate(a.date);
-	});
+
+	const sortedPosts = posts
+		.sort((a, b) => {
+			const parseDate = (dateString: string) => {
+				const [day, month, year] = dateString.split('.').map(Number);
+				return new Date(year, month - 1, day).getTime();
+			};
+			return parseDate(b.date) - parseDate(a.date);
+		})
+		.slice(0, 5);
+
 	return {
 		posts: sortedPosts
 	};
