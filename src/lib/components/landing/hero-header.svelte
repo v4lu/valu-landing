9bd71f5280c766c91ea2b93d741cbc7398ebc54a
Luka <lukabrkovic@proton.me>
2024-10-10 08:55:27 +0200

    feat: fly animation

--- src/lib/components/landing/hero-header.svelte
+++ src/lib/components/landing/hero-header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
 	import { Button } from '../ui/button';
 	import { Heading } from '../ui/heading';
 	import { scrollToSection } from '$lib/cn';
@@ -7,16 +7,16 @@
 
 <div class="overflow-hidden">
 	<div class="transform-gpu py-12">
-		<div in:fade={{ duration: 300, delay: 300 }}>
+		<div in:fly={{ y: 50, duration: 300, delay: 300 }}>
 			<Heading size="hero">Let us build great product for you!</Heading>
 		</div>
-		<div in:fade={{ duration: 300, delay: 600 }}>
+		<div in:fly={{ y: 50, duration: 300, delay: 600 }}>
 			<p class="mt-4 max-w-3xl text-pretty text-muted-foreground lg:text-xl">
 				Transform your ideas into reality with our expert team. We craft innovative solutions tailored to your unique
 				needs, ensuring your product stands out in the market.
 			</p>
 		</div>
-		<div in:fade={{ duration: 300, delay: 900 }}>
+		<div in:fly={{ y: 50, duration: 300, delay: 900 }}>
 			<div class="mt-8 space-x-4">
 				<Button
 					onclick={(e) => scrollToSection(e, 'contact', -50)}
