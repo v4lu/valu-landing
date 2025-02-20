281828ac787dcb8df92db1ce2187a425a10e9ba4
Luka <lukabrkovic@proton.me>
2025-02-20 15:26:24 +0100

    fix loading blogs

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -12,8 +12,8 @@ type Metadata = {
 };
 
 export const load: PageServerLoad = async ({ params }) => {
-	const postsDirectory = join(process.cwd(), 'build', 'src', 'blogs');
-	// const postsDirectory = join(process.cwd(), 'src', 'blogs');
+	// const postsDirectory = join(process.cwd(), 'build', 'src', 'blogs');
+	const postsDirectory = join('src', 'blogs');
 	const filePath = join(postsDirectory, `${params.slug}.md`);
 
 	const fileContent = await readFile(filePath, 'utf-8');
@@ -35,7 +35,7 @@ export const load: PageServerLoad = async ({ params }) => {
 		};
 	} catch (error) {
 		console.error('Error parsing frontmatter:', error);
-		console.log('Frontmatter content:', frontmatter);
+		console.error('Frontmatter content:', frontmatter);
 		throw error;
 	}
 };
